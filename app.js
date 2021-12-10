788f2b083de45b70eaf3fd2084f8c853772f3b46
Lemmy Mwaura <lemminton@gmail.com>
2021-12-10 17:41:57 +0300

    A few improvements

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 
-function runCode(){
-    let enterDate = prompt("Enter Date");
-    console.log(enterDate)
-}
+const submit = document.getElementById("submit")
+const form = document.getElementById("form")
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const date = document.getElementById('date').value
+    const month = document.getElementById('month').value
+    const year = document.getElementById('year').value
+    const genderInput = document.querySelector('input[name="gender"]:checked').value
+
+    console.log(date)
+    console.log(month)
+    console.log(year)
+    console.log(genderInput)
+})
+
+console.log(date)
+
+// function runCode(){
+//     let enterDate = prompt("Enter Date")
+//     console.log(enterDate)
+// }
