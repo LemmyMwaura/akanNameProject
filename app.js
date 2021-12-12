3453830b9264ba1575e39abe09305f976a8934f4
Lemmy Mwaura <lemminton@gmail.com>
2021-12-12 13:34:22 +0300

    Added image file, Styled the project

--- app.js
+++ app.js
@@ -7,28 +7,28 @@ form.addEventListener("submit", (event) => {
   const year = document.getElementById("year").value
   const male = document.querySelector("#male")
   const female = document.querySelector("#female")
-
+ 
   checkValidity()
 })
 
+
 function checkValidity(event) {
-  if (date.value.length == 0 && month.value.length == 0) {
-    alert("Please Enter your Date of Birth")
-  } else {
+  
     if (date.value > 31 || date.value <= 0) {
       alert("Enter the correct date value")
     }
-    if (month.value > 12 || month.value <= 0) {
+    else if (month.value > 12 || month.value <= 0) {
       alert("Enter the correct month value")
-    }
-    if (year.value > 2021) {
+    } 
+    else if (year.value > 2021) {
       alert("You cannot be born in the future")
-    } else if (year.value <= 0) {
+    }
+     else if (year.value <= 0) {
       alert("Please enter correct Year of Birth")
-    } else {
+    } 
+    else {
       checkGender()
     }
-  }
 }
 
 function calcDayOfWeek() {
@@ -43,10 +43,12 @@ function calcDayOfWeek() {
 }
 
 function checkGender() {
+  const name = document.querySelector("#name").value
+
   if (female.checked == true) {
-    alert(`Your AkanName is ${pickFemaleAkam(calcDayOfWeek)}`)
+    alert(`Hi ${name}, your AkanName is ${pickFemaleAkam(calcDayOfWeek)}`)
   } else if (male.checked == true) {
-    alert(`Your AkanName is ${pickMaleAkam(calcDayOfWeek)}`)
+    alert(`Hi ${name}, your AkanName is ${pickMaleAkam(calcDayOfWeek)}`)
   } else {
     alert("Please select Gender")
   }
@@ -89,3 +91,7 @@ function pickMaleAkam(day) {
       return "Kofi"
   }
 }
+
+//Container slider
+const bullets = document.querySelectorAll('.bullets span');
+const images = document.querySelectorAll('.image')
